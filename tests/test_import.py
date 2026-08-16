32c589882e529b34b00da43d4877a861de1f3705
only-dev-time <111915936+only-dev-time@users.noreply.github.com>
2026-08-16 21:46:44 +0200

    Add cli execution test

--- tests/test_import.py
+++ tests/test_import.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 import os
+import subprocess
 import sys
-sys.path.insert(0, os.path.abspath('..'))
+
+
+sys.path.insert(0, os.path.abspath(".."))
 
 from steem import *  # noqa
 from steembase import *  # noqa
@@ -11,3 +14,14 @@ from steembase import *  # noqa
 def test_import():
     _ = Steem()
     _ = account.PasswordKey
+
+
+def test_cli_help():
+    result = subprocess.run(
+        ["steempy", "-h"],
+        capture_output=True,
+        text=True,
+    )
+
+    assert result.returncode == 0
+    assert "usage:" in result.stdout.lower()
